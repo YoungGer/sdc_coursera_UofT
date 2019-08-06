1b61aecc10390dca58e1f023f12f7c99da4fa0bf
yangguangyao <yangguangyao@baidu.com>
2019-08-06 14:49:13 +0800

    path generation

--- CarlaSimulator/PythonClient/Course4FinalProject/path_optimizer.py
+++ CarlaSimulator/PythonClient/Course4FinalProject/path_optimizer.py
@@ -74,7 +74,7 @@ class PathOptimizer:
         # has a lower limit of the straight line arc length.
         # TODO: INSERT YOUR CODE BETWEEN THE DASHED LINES
         # ------------------------------------------------------------------
-        # bounds = ...
+        bounds = [[-0.5, 0.5], [-0.5, 0.5], [sf_0, np.inf]]
         # ------------------------------------------------------------------
 
         # Here we will call scipy.optimize.minimize to optimize our spiral.
@@ -84,7 +84,7 @@ class PathOptimizer:
         # optimization methods.
         # TODO: INSERT YOUR CODE BETWEEN THE DASHED LINES
         # ------------------------------------------------------------------
-        # res = scipy.optimize.minimize(...)
+        res = scipy.optimize.minimize(self.objective, p0, method='L-BFGS-B', jac=self.objective_grad, constraints=bounds)
         # ------------------------------------------------------------------
 
         spiral = self.sample_spiral(res.x)
@@ -110,14 +110,11 @@ class PathOptimizer:
     #         c - the third term of kappa(s).
     #         d - the fourth term of kappa(s).
     def thetaf(self, a, b, c, d, s):
-        pass
-
         # TODO: INSERT YOUR CODE BETWEEN THE DASHED LINES
         # ------------------------------------------------------------------
-        # # Remember that a, b, c, d and s are lists
-        # ...
-        # thetas = ...
-        # return thetas
+        # Remember that a, b, c, d and s are lists
+        thetas = [a*s + b/2*s**2 + c/3*s**3 + d/4*s**4 for x in s]
+        return thetas
         # ------------------------------------------------------------------
 
     ######################################################
@@ -171,10 +168,10 @@ class PathOptimizer:
         # Try to vectorize the code using numpy functions for speed if you can.
         # TODO: INSERT YOUR CODE BETWEEN THE DASHED LINES
         # ------------------------------------------------------------------
-        # t_points = ...
-        # x_points = ...
-        # y_points = ...
-        # return [x_points, y_points, t_points]
+        t_points = self.thetaf(a, b, c, d, s_points)
+        x_points = scipy.integrate.cumtrapz(np.cos(t_points), s_points, initial=0)
+        y_points = scipy.integrate.cumtrapz(np.sin(t_points), s_points, initial=0)
+        return [x_points, y_points, t_points]
         # ------------------------------------------------------------------
 
     ######################################################
